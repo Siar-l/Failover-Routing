4fcd6b7719a447e6167af99f93caa58ae478d848
Siar <farazpamir@outlook.com>
2025-10-31 11:47:36 +0100

    dinamic failure generate

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ def create_graph():
 
 def build_spanning_trees(G, target, k):
     trees = []
-    for i in range(k):
+    for _ in range(k):
 
         for(u, v)in G.edges():
              G[u][v] ['weight'] = random.random()
 
-             T = nx.minimum_spanning_tree(G)
-             trees.append(T)
-        return trees
+        T = nx.minimum_spanning_tree(G)
+        trees.append(T)
+    return trees
     
     
 def simulate_failover(G, trees, source, target,  faild_edges, k):
@@ -45,20 +45,44 @@ def simulate_failover(G, trees, source, target,  faild_edges, k):
         current_node = next_node
     return True, bounces
 
+def update_failures(G, failed_edges,failure_rate=0.2, recovery_rate=0.1):
+    edges = list(G.edges())
+
+    for e in edges:
+         if e not in failed_edges and random.random() < failure_rate:
+              failed_edges.add(e)
+              failed_edges.add((e[1], e[0]))
+              for e in list(failed_edges):
+                   if random.random() < recovery_rate:
+                        failed_edges.discard(e)
+                        failed_edges.discard((e[1],e[0]))
+    return failed_edges       
+def dynamic_simulation(G, trees, source, target, k, steps=10):
+     failed_edges = set()
+     for t in range(steps):
+          print(f"\n Zeit = {t+1}")
+          failed_edges = update_failures(G, failed_edges)
+          success, bounces = simulate_failover(G, trees,source, target, failed_edges, k)
+
+          print("-> Routing erfolgreich:", success)
+          print("Bounce:", bounces)
+          print("-> Aktuel defekte Kanten:", failed_edges)
+
 
 def main():
-     k = 3 
+     k = 10
      G = create_graph()
      target = 0
      source = 5 
 
      trees = build_spanning_trees(G, target, k)
-     failed_edges = random.sample(list(G.edges()),2)
+     dynamic_simulation(G, trees, source, target, k, steps=10)
+     #failed_edges = random.sample(list(G.edges()),2)
 
-     success, bounces = simulate_failover(G, trees, source, target, failed_edges,k)
-     print("Erfolg:", success)
-     print("Bounce:", bounces)
-     print("Ausgefallene Kanten", failed_edges)
+     #success, bounces = simulate_failover(G, trees, source, target, k, steps=10)
+     #print("Erfolg:", success)
+     #print("Bounce:", bounces)
+     #print("Ausgefallene Kanten", failed_edges)
 
 if    __name__=="__main__":
      main()
